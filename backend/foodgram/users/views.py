0ab79134ba1fbace2d704b9a45f6396cc0f291b7
LuSan <lusanuri@gmail.com>
2023-07-24 23:28:05 +0300

    пагинация и api usurs

--- backend/foodgram/users/views.py
+++ backend/foodgram/users/views.py
@@ -4,13 +4,11 @@ from rest_framework import viewsets, status
 from rest_framework.decorators import action, api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
-from .serializers import CustomUserSerializer
 
 User = get_user_model()
 
 class CustomUserViewSet(UserViewSet):
     queryset = User.objects.all()
-    serializer_class = CustomUserSerializer
 
     @action(detail=False, permission_classes=[IsAuthenticated],)
     def subscriptions(self, request):
