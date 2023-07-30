099c8e215ec34784a9cff3cb26266fd64efdb9bc
LuSan <lusanuri@gmail.com>
2023-07-30 14:50:14 +0300

    1-2

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -83,14 +83,22 @@ DATABASES = {
 }
 
 AUTH_PASSWORD_VALIDATORS = [
-    {'NAME':
-     'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
-    {'NAME': 
-     'django.contrib.auth.password_validation.MinimumLengthValidator'},
-    {'NAME': 
-     'django.contrib.auth.password_validation.CommonPasswordValidator'},
-    {'NAME': 
-     'django.contrib.auth.password_validation.NumericPasswordValidator'},
+    {
+        'NAME':
+            'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME':
+            'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME':
+            'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME':
+            'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
 ]
 
 LANGUAGE_CODE = 'en-us'
@@ -110,7 +118,8 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
     ],
-    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "DEFAULT_PAGINATION_CLASS":
+    "rest_framework.pagination.PageNumberPagination",
     'PAGE_SIZE': 6,
 }
 
@@ -128,7 +137,8 @@ DJOSER = {
     },
 }
 
-CSRF_TRUSTED_ORIGINS = os.environ.get("CSRF_TRUSTED_ORIGINS", "http://localhost").split(",")
+CSRF_TRUSTED_ORIGINS = os.environ.get(
+    "CSRF_TRUSTED_ORIGINS", "http://localhost").split(",")
 
 STATIC_URL = '/static/'
 
